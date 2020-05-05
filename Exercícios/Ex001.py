cc0e049f5593ccd89b2344dbf5a06432083532e6
Lucas-Moura-Da-Silva <lucas.silva566949@gmail.com>
2020-05-05 13:41:16 -0300

    exercícios de python

---
+++ Exercícios/Ex001.py
@@ -0,0 +1,11 @@
+#1) Crie um script em Python que solicite o nome, altura e peso de uma pessoa e mostre a seguinte
+# mensagem: “João tem 90 kilos e altura de 1.78 e portanto o IMC é de 28.4”.
+
+nome = input('Qual é o seu nome?')
+alt = float(input('Qual a sua altura em metros?'))
+peso = float(input('Qual é o seu peso em Kg?'))
+
+#Calculo do IMC
+imc = peso / (alt)**2
+
+print(f'{nome} tem {peso}Kg e altura de {alt}m e portanto o IMC é de {imc:.2f}.')
---
+++ Exercícios/Ex002.py
@@ -0,0 +1,9 @@
+#2) Escreva um script que leia um valor em metros e o exiba convertido em milímetros
+
+metro = float(input('Qual valor em metros(m) você quer converter em centimetros(cm)?'))
+
+#calculo de converção
+cm = metro * 100
+
+#converção de metros para centimetros
+print(f'O valor de {metro}m cofertido ficará em {cm}cm.')
